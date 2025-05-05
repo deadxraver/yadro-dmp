b850e18a64c0eb416a44f153bcecfce46ee5d5ee
deadxraver <409853@edu.itmo.ru>
2025-05-05 23:10:02 +0300

    first working version of kernel module yay

---
+++ README.md
@@ -0,0 +1,14 @@
+# User Guide (Ubuntu/Debian)
+
+## Install packages
+
+```bash
+sudo apt-get install build-essential kmod
+```
+
+## Build & insert
+
+```bash
+make
+sudo insmod dmp.ko
+```
---
+++ dmp.c
@@ -0,0 +1,11 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int init_module(void) {
+  printk(KERN_INFO "Hello lol\n");
+  return 0;
+}
+
+void cleanup_module(void) { pr_info("bye-bye\n"); }
+
+MODULE_LICENSE("GPL");
