e71225739c17c9cab3c97172539107f89f39080a
deadxraver <409853@edu.itmo.ru>
2025-05-06 20:56:42 +0300

    structure for read and write info

--- dmp.c
+++ dmp.c
@@ -1,13 +1,21 @@
+#include <linux/atomic.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+struct dmp_info {
+  atomic_t read_requests;
+  atomic_t write_requests;
+  atomic_t read_size;
+  atomic_t write_size
+};
+
 int init_module(void) {
-  printk(KERN_INFO "Hello lol\n");
+  printk(KERN_INFO "dmp init\n");
   return 0;
 }
 
 void cleanup_module(void) {
-  pr_info("bye-bye\n");
+  printk(KERN_INFO "dmp drop\n");
 }
 
 MODULE_LICENSE("GPL");
