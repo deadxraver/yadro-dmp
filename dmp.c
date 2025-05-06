f0119579ded94c7226fb34195f3a302db17f7d75
deadxraver <409853@edu.itmo.ru>
2025-05-06 18:18:54 +0300

    it took me 3 hours to make nvim use my .clang-format config

--- dmp.c
+++ dmp.c
@@ -2,10 +2,13 @@
 #include <linux/module.h>
 
 int init_module(void) {
+  int* test;
   printk(KERN_INFO "Hello lol\n");
   return 0;
 }
 
-void cleanup_module(void) { pr_info("bye-bye\n"); }
+void cleanup_module(void) {
+  pr_info("bye-bye\n");
+}
 
 MODULE_LICENSE("GPL");
